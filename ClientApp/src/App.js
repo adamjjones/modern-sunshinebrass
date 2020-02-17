be16366de45013570740647f8626a72ddb39983f
Adam Jones <adam@linuxtampa.com>
2020-02-16 23:27:20 -0500

    Navbar and mobile menu are driven from one array.

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Redirect, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Redirect, Route, Link } from 'react-router-dom'
 import { Dropdown } from 'react-bootstrap'
-import Navbar from './components/Navbar'
 import Banner from './components/Banner'
 import HomePage from './components/HomePage'
 import Concerts from './components/Concerts'
@@ -11,9 +10,23 @@ import Rehearsals from './components/Rehearsals'
 import Members from './components/Members'
 import History from './components/History'
 import Contact from './components/Contact'
+// import Photos from './components/Photos'
 import Videos from './components/Videos'
 import Links from './components/Links'
 
+const components = [
+  { url: "/HomePage", label: "Home", component: HomePage },
+  { url: "/Concerts", label: "Concerts", component: Concerts },
+  { url: "/Rehearsals", label: "Rehearsals", component: Rehearsals },
+  { url: "/Christmas", label: "Christmas CD", component: Christmas },
+  { url: "/BrassBand", label: "Brass Band CD", component: BrassBand },
+  { url: "/Members", label: "Members", component: Members },
+  // { url: "/Photos", label: "Photos", component: Photos },
+  { url: "/Videos", label: "Videos", component: Videos },
+  { url: "/History", label: "History", component: History },
+  { url: "/Contact", label: "Contact Us", component: Contact },
+  { url: "/Links", label: "Links", component: Links }
+]
 
 class App extends Component {
   render() {
@@ -25,32 +38,36 @@ class App extends Component {
           </Dropdown.Toggle>
 
           <Dropdown.Menu className="menu">
-            <Dropdown.Item className="menu-item" href="/HomePage">Home</Dropdown.Item>
-            <Dropdown.Item className="menu-item" href="/Concerts">Concerts</Dropdown.Item>
-            <Dropdown.Item className="menu-item" href="/Christmas">Christmas CD</Dropdown.Item>
-            <Dropdown.Item className="menu-item" href="/BrassBand">BrassBand CD</Dropdown.Item>
-            <Dropdown.Item className="menu-item" href="/Rehearsals">Rehearsals</Dropdown.Item>
-            <Dropdown.Item className="menu-item" href="/Members">Members</Dropdown.Item>
-            <Dropdown.Item className="menu-item" href="/Photos">Photos</Dropdown.Item>
-            <Dropdown.Item className="menu-item" href="/Videos">Videos</Dropdown.Item>
-            <Dropdown.Item className="menu-item" href="/History">History</Dropdown.Item>
-            <Dropdown.Item className="menu-item" href="/Links">Links</Dropdown.Item>
+            {
+              components.map(c => {
+                return <Dropdown.Item className="menu-item" href={c.url}>{c.label}</Dropdown.Item>
+
+              })
+            }
           </Dropdown.Menu>
         </Dropdown>
           <Banner />
-          <Navbar />
+          {/* <Navbar components={components} /> */}
+          <div className="nav-wrapper">
 
+            <nav className="center">
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
+                {
+                  components.map(c => {
+                    return (
+                      <li><Link to={c.url}>{c.label}</Link></li>
+                    )
+                  })
+                }
+              </ul>
+            </nav>
+          </div>
           <Switch>
-            <Route path="/HomePage" exact component={HomePage} />
-            <Route path="/Concerts" exact component={Concerts} />
-            <Route path="/Christmas" exact component={Christmas} />
-            <Route path="/BrassBand" exact component={BrassBand} />
-            <Route path="/Rehearsals" exact component={Rehearsals} />
-            <Route path="/Members" exact component={Members} />
-            <Route path="/Videos" exact component={Videos} />
-            <Route path="/History" exact component={History} />
-            <Route path="/Contact" exact component={Contact} />
-            <Route path="/Links" exact component={Links} />
+            {
+              components.map(c => {
+                return <Route path={c.url} exact component={c.component} />
+              })
+            }
             <Redirect from="/" to="HomePage" />
           </Switch>
         </Router>
