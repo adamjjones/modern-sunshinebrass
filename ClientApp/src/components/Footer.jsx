3336f1e9e5804dd6b79d30f7f9871b57f08f6ff5
Adam Jones <adam@linuxtampa.com>
2020-03-24 17:21:11 -0400

    Fixed a few CSS issues

--- ClientApp/src/components/Footer.jsx
+++ ClientApp/src/components/Footer.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="footer">
-      <p>Made with &hearts; in Temple Terrace, FL</p>
+      <div>Made with &hearts; by Adam Jones</div>
+      <div>
+        Portfolio: <a href="https://admjones.tech">adamjones.tech</a>
+        &nbsp;&nbsp;<a href="https://github.com/adamjjones">Github</a>
+      </div>
     </div>
   )
 }
--- ClientApp/src/components/Members.jsx
+++ ClientApp/src/components/Members.jsx
@@ -4,9 +4,13 @@ class Members extends Component {
   render() {
     return (
       <div className="content">
-        <p>Council of Hillsborough County.</p>
+        <h4>Interested in Joining?</h4>
+        <p>
+          NOTE: The Sunshine Brass Band Inc. is a not for profit
+          corporation, supported in part by a grant
+          from the Arts Council of Hillsborough County.</p>
         <p>The band's members are from throughout the Tampa Bay area, and among its ranks can be found teachers, professors, software developers, students, retirees, engineers, architects, university and government administrators.</p>
-        <p>The Sunshine Brass Band is currently seeking talented brass and percussion players wishing to help us grow our ensemble. For more information about auditions, please contact us.</p>
+        <p>The Sunshine Brass Band is currently seeking talented brass and percussion players wishing to help us grow our ensemble. For more information about auditions, please contact Tim Bailey-Jones at (813) 900-8131.</p>
         <p className="center">The Sunshine Brass Band Inc. is a not for profit corporation.</p>
       </div>
     );
