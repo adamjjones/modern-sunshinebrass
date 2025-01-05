7751d7fede7b88e08a41d73d997e241249918cc7
Tim Bailey-Jones <tim@bailey-jones.com>
2025-01-05 04:50:08 -0500

    automated the adding of my public resume to the deploy process

---
+++ ClientApp/extra-files-to-deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+gcloud storage cp -r build/index.html gs://sunshinebrass.com/Christmas
+gcloud storage cp -r build/index.html gs://sunshinebrass.com/Concerts
+gcloud storage cp -r build/index.html gs://sunshinebrass.com/Rehearsals
+gcloud storage cp -r build/index.html gs://sunshinebrass.com/BrassBand
+gcloud storage cp -r build/index.html gs://sunshinebrass.com/Members
+gcloud storage cp -r build/index.html gs://sunshinebrass.com/Photos
+gcloud storage cp -r build/index.html gs://sunshinebrass.com/Videos
+gcloud storage cp -r build/index.html gs://sunshinebrass.com/History
+gcloud storage cp -r build/index.html gs://sunshinebrass.com/Contact
+gcloud storage cp -r build/index.html gs://sunshinebrass.com/Links
